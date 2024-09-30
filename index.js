47ff5649243150fd5345b0492f22d19c18468c01
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-09-30 20:18:13 +0530

    Add

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
+import path from 'path';
+import * as url from "url";
+const directoryUrl = new URL(".", import.meta.url);
+const directoryPath = url.fileURLToPath(directoryUrl);
 const app = express();
-app.use(bodyParser.urlencoded())
-app.post('/home',(req,res)=>{
-  res.send("your  name is " + req.body.name);
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.set("view engine","ejs");
+app.use(express.static(path.join(directoryPath,'public')));//use for front End
+app.get('/',(req,res)=>{
+  res.render("index");  
+})
+app.get('/:name',(req,res)=>{
+  res.send("name is "+ req.params.name)
 })
 app.listen(3000,()=>{
-  console.log(chalk.white.bgGreen.bold("server is running in port number")+ chalk.green(" 3000"))
+  console.log(chalk.bgGreen.bold("server is running in port number")+ chalk.green(" 3000"))
 });
