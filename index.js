130fc80d459d612f78b6c4db47af57e1436a28c1
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-17 12:39:21 +0530

    add

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.set('views', path.join(directoryPath,'views'));
 app.use(express.static(path.join(directoryPath,'public')));//use for front End
 app.get('/',(req,res)=>{
-  res.render("index.ejs");  
+  res.render("views/index.ejs");  
 })
 app.get('/:name',(req,res)=>{
   res.send("name is "+ req.params.name)
