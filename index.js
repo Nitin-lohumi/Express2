8a1648028d898a6129606f8613d6201776a2705a
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-17 12:44:35 +0530

    add

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const directoryPath = url.fileURLToPath(directoryUrl);
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
-app.set('views', path.join(directoryPath,'views'));
 app.set("view engine","ejs");
+app.set('views', path.join(directoryPath,'views'));
 app.use(express.static(path.join(directoryPath,'public')));//use for front End
 app.get('/',(req,res)=>{
-  res.render("index");  
+  res.render("./index.ejs");  
 })
 app.get('/:name',(req,res)=>{
   res.send("name is "+ req.params.name)
